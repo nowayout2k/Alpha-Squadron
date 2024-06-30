4db618dee17efaec4a7eadbd98e5f5c689cdc4fb
Johnnie Otis <johnnie@mod.io>
2024-06-29 22:22:50 -0700

    no message

---
+++ Header/EntityType.h
@@ -0,0 +1,17 @@
+//
+// Created by Johnnie Otis on 6/29/24.
+//
+
+#ifndef ENTITYTYPE_H_
+#define ENTITYTYPE_H_
+
+enum EntityType
+{
+	None = 0x0,
+	Player = 0x1,
+	Enemy = 0x2,
+	Projectile = 0x4,
+	Background = 0x8,
+};
+
+#endif //ENTITYTYPE_H_
