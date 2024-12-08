ecf58c548cfb7110d76ee6b5a30115d2e2b3ff0f
Johnnie Otis <otista_2000@hotmail.com>
2024-12-07 16:29:25 -0800

    refactor Ai

--- Game/Aircraft.cpp
+++ Game/Aircraft.cpp
@@ -1,35 +1,36 @@
-//
-// Created by Johnnie Otis on 6/9/24.
-//
-
 #include "Aircraft.h"
 #include "../Engine/Audio.h"
 #include "../Engine/Projectile.h"
 #include "../Engine/World.h"
 #include "../Engine/Engine.h"
 
-#define DAMAGE_FLASH_TIME 4.0f
-#define DAMAGE_INVINCIBILITY_TIME .3f
-#define FIRE_COOLDOWN_TIME 0.2f
+constexpr float DAMAGE_FLASH_TIME = 4.0f;
+constexpr float DAMAGE_INVINCIBILITY_TIME = 0.3f;
+constexpr float FIRE_COOLDOWN_TIME = 0.2f;
+constexpr float MAX_SPAWN_DISTANCE = 1000.0f;
+
 #ifndef M_PI
-#define M_PI 3.14159265359
+	#define M_PI 3.14159265359
 #endif
 
 std::vector<AircraftData> Aircraft::m_aircraftData = LoadAircraftData("../Game/DataFiles/aircraftData.json");
 
-Aircraft::Aircraft(const bool hasCollision, sf::Vector2f scale, sf::Vector2f position) : GameSprite(hasCollision)
+Aircraft::Aircraft(const bool hasCollision, sf::Vector2f scale, sf::Vector2f position)
+	: GameSprite(hasCollision),
+	  m_isExiting(false),
+	  m_timeSinceDamage(0),
+	  m_routineDistanceTravelled(0),
+	  m_spawnDistanceTravelled(0),
+	  m_routineIndex(0),
+	  m_isDamageAnimationActive(false),
+	  m_fireCooldownRemaining(0),
+	  m_spawnPos(position)
 {
-	m_isMovingToStartPos = true;
-	m_timeSinceDamage = 0;
-	m_routineDistanceTravelled = 0;
-	m_spawnDistanceTravelled = 0;
-	m_routineIndex = 0;
-	m_isDamageAnimationActive = false;
-	m_fireCooldownRemaining = 0;
-	m_spawnPos = position;
 	setScale(scale);
 	setPosition(m_spawnPos);
-	std::unique_ptr<GameText> healthDisplay(new GameText(FontId::Gamer, "", 12, sf::Color::Black, sf::Text::Style::Regular, sf::Vector2f()));
+
+	auto healthDisplay = std::make_unique<GameText>(
+		FontId::Gamer, "", 12, sf::Color::Black, sf::Text::Style::Regular, sf::Vector2f());
 	m_healthDisplay = healthDisplay.get();
 	attachNode(std::move(healthDisplay));
 }
@@ -37,18 +38,18 @@ Aircraft::Aircraft(const bool hasCollision, sf::Vector2f scale, sf::Vector2f pos
 void Aircraft::takeDamage(int health)
 {
 	m_health -= health;
-	if(m_isDamageAnimationActive || m_health <= 0)
+	if (m_isDamageAnimationActive || m_health <= 0)
 	{
 		Audio::playSound(SoundFxId::Explosion, 50);
 		destroy();
 		return;
 	}
+
 	setColor(sf::Color::Red);
 	Audio::playSound(SoundFxId::Damage, 50);
 	m_timeSinceDamage = 0;
 	m_isDamageAnimationActive = true;
 }
-
 
 void Aircraft::handleAnimation(float deltaTime)
 {
@@ -57,22 +58,23 @@ void Aircraft::handleAnimation(float deltaTime)
 
 void Aircraft::fireBullet(sf::Vector2f velocity)
 {
-	if(m_fireCooldownRemaining > 0)
+	if (m_fireCooldownRemaining > 0)
 		return;
 
-	auto offset = sf::Vector2f(0, getGlobalBounds().height/2);
-	auto pro = std::make_unique<Projectile>((NodeType)getNodeType(), offset, velocity);
+	auto offset = sf::Vector2f(0, getGlobalBounds().height / 2);
+	auto projectile = std::make_unique<Projectile>((NodeType)getNodeType(), offset, velocity);
 	m_fireCooldownRemaining = FIRE_COOLDOWN_TIME;
-	pro->loadStateResources();
-	attachNode(std::move(pro));
+	projectile->loadStateResources();
+	attachNode(std::move(projectile));
 }
 
 void Aircraft::handleDamageAnimation(float deltaTime)
 {
-	if(m_isDamageAnimationActive)
+	if (m_isDamageAnimationActive)
 	{
 		m_timeSinceDamage += deltaTime;
-		if(m_timeSinceDamage > DAMAGE_FLASH_TIME)
+
+		if (m_timeSinceDamage > DAMAGE_FLASH_TIME)
 		{
 			setColor(sf::Color::White);
 			m_isDamageAnimationActive = false;
@@ -80,10 +82,11 @@ void Aircraft::handleDamageAnimation(float deltaTime)
 		else
 		{
 			float phase = fmod(m_timeSinceDamage, 1.0f);
-			float t = 0.5 * (1.0f + std::cos(phase * 2.0f * M_PI));
+			float t = 0.5f * (1.0f + std::cos(phase * 2.0f * M_PI));
 			float c = Utility::lerp(0, 255, t);
-			setColor(sf::Color(255,c,c,255));
+			setColor(sf::Color(255, c, c, 255));
 		}
+
 		setCollision(m_timeSinceDamage > DAMAGE_INVINCIBILITY_TIME);
 	}
 }
@@ -91,110 +94,102 @@ void Aircraft::handleDamageAnimation(float deltaTime)
 void Aircraft::update(float deltaTime)
 {
 	Entity::update(deltaTime);
+	updateHealthDisplay();
 
-	m_healthDisplay->setString(std::to_string(m_health) + " HP");
-	auto bounds = GameSprite::getLocalBounds();
-	m_healthDisplay->setPosition(bounds.width/2, -bounds.height/2);
-	m_healthDisplay->setRotation(-getRotation());
-
-	if(m_fireCooldownRemaining > 0)
+	if (m_fireCooldownRemaining > 0)
 		m_fireCooldownRemaining -= deltaTime;
 
-	if(!(getNodeType() & static_cast<unsigned int>(NodeType::Player)))
+	if (!(getNodeType() & static_cast<unsigned int>(NodeType::Player)))
 	{
-		auto pos = getPosition();
-		Debug::log("CURRENT pos x:" + std::to_string(pos.x) + " y:" + std::to_string(pos.y));
+		updatePosition(deltaTime);
+	}
+}
 
-		auto& view = Engine::getWindow().getView();
-		sf::Vector2f center = view.getCenter();
-		sf::Vector2f size = view.getSize();
+void Aircraft::updateHealthDisplay()
+{
+	m_healthDisplay->setString(std::to_string(m_health) + " HP");
+	auto bounds = GameSprite::getLocalBounds();
+	m_healthDisplay->setPosition(bounds.width / 2, -bounds.height / 2);
+	m_healthDisplay->setRotation(-getRotation());
+}
 
-		auto viewRect = sf::FloatRect(center - size / 2.f, size);
+void Aircraft::updatePosition(float deltaTime)
+{
+	auto& view = Engine::getWindow().getView();
+	sf::Vector2f viewSize = view.getSize();
+	sf::Vector2f viewCenter = view.getCenter();
+	auto viewRect = sf::FloatRect(viewCenter - viewSize / 2.f, viewSize);
 
-		if(viewRect.left + viewRect.width > m_spawnPos.x + m_despawnDistance)
-			m_isExiting = true;
+	if (viewRect.left + viewRect.width > m_spawnPos.x + m_despawnDistance)
+		m_isExiting = true;
 
-		if(m_spawnDistanceTravelled > 500)
-			m_isMovingToStartPos = false;
+	if (m_spawnDistanceTravelled < MAX_SPAWN_DISTANCE)
+	{
+		moveTowardsStart(deltaTime);
+	}
+	else if (m_isExiting)
+	{
+		exitPhase();
+	}
+	else if (!m_aiRoutines.empty())
+	{
+		followAiRoutines(deltaTime);
+	}
+}
 
-		if(m_isMovingToStartPos)
-		{
-			Debug::log("ai Enter called");
-			float vx = 0;
-			float vy = 0;
-			switch (m_enterDirection)
-			{
-			case Direction::North:
-				vx = 0;
-				vy = -getMaxSpeed();
-				break;
-			case Direction::South:
-				vx = 0;
-				vy = getMaxSpeed();
-				break;
-			case Direction::East:
-				vx = World::getScrollSpeed() + getMaxSpeed();
-				vy = 0;
-				break;
-			case Direction::West:
-				vx = World::getScrollSpeed() - getMaxSpeed();
-				vy = 0;
-				break;
-			}
-			setVelocity(sf::Vector2f(vx, vy));
-			m_spawnDistanceTravelled += getMaxSpeed() * deltaTime;
-		}
-		else if(m_isExiting)
-		{
-			Debug::log("ai Exit called");
-			float vx = 0;
-			float vy = 0;
-			switch (m_exitDirection)
-			{
-			case Direction::North:
-				vx = 0;
-				vy = -getMaxSpeed();
-				break;
-			case Direction::South:
-				vx = 0;
-				vy = getMaxSpeed();
-				break;
-			case Direction::East:
-				vx = World::getScrollSpeed() + getMaxSpeed();
-				vy = 0;
-				break;
-			case Direction::West:
-				vx = World::getScrollSpeed() - getMaxSpeed();
-				vy = 0;
-				break;
-			}
-			setVelocity(sf::Vector2f(vx, vy));
-		}
-		else if(m_aiRoutines.size() > 0)
-		{
-			float distanceToTravel = m_aiRoutines[m_routineIndex].distance;
-			if (m_routineDistanceTravelled > distanceToTravel)
-			{
-				m_routineIndex = (m_routineIndex + 1) % m_aiRoutines.size();
-				m_routineDistanceTravelled = 0.f;
-			}
-			float radians = Utility::toRadian(m_aiRoutines[m_routineIndex].angle);
-			float vx = World::getScrollSpeed() + getMaxSpeed() * std::cos(radians);
-			float vy = getMaxSpeed() * std::sin(radians);
-			Debug::log("Set Ai Velocity to x: " + std::to_string(vx) + " Y:" +std::to_string(vy) + " sin:" +  std::to_string(std::sin(radians)) + " cos:" + std::to_string(std::cos(radians)));
-			setVelocity(sf::Vector2f(vx, vy));
-			m_routineDistanceTravelled += getMaxSpeed() * deltaTime;
-		}
+void Aircraft::moveTowardsStart(float deltaTime)
+{
+	sf::Vector2f velocity = calculateDirectionalVelocity(m_enterDirection);
+	setVelocity(velocity);
+	m_spawnDistanceTravelled += getMaxSpeed() * deltaTime;
+}
+
+void Aircraft::exitPhase()
+{
+	sf::Vector2f velocity = calculateDirectionalVelocity(m_exitDirection);
+	setVelocity(velocity);
+}
+
+void Aircraft::followAiRoutines(float deltaTime)
+{
+	float distanceToTravel = m_aiRoutines[m_routineIndex].distance;
+	if (m_routineDistanceTravelled > distanceToTravel)
+	{
+		m_routineIndex = (m_routineIndex + 1) % m_aiRoutines.size();
+		m_routineDistanceTravelled = 0.0f;
+	}
+
+	float radians = Utility::toRadian(m_aiRoutines[m_routineIndex].angle);
+	sf::Vector2f velocity = sf::Vector2f(
+		World::getScrollSpeed() + getMaxSpeed() * std::cos(radians),
+		getMaxSpeed() * std::sin(radians));
+
+	setVelocity(velocity);
+	m_routineDistanceTravelled += getMaxSpeed() * deltaTime;
+}
+
+sf::Vector2f Aircraft::calculateDirectionalVelocity(Direction direction) const
+{
+	switch (direction)
+	{
+	case Direction::North:
+		return sf::Vector2f(0, -getMaxSpeed());
+	case Direction::South:
+		return sf::Vector2f(0, getMaxSpeed());
+	case Direction::East:
+		return sf::Vector2f( getMaxSpeed() + World::getScrollSpeed(), 0);
+	case Direction::West:
+		return sf::Vector2f(-(getMaxSpeed() - World::getScrollSpeed()), 0);
+	default:
+		return sf::Vector2f(0, 0);
 	}
 }
 
 void Aircraft::loadResources()
 {
-	Debug::log("Aircraft Type: " + Utility::aircraftTypeToString(getAircraftType()));
-	for (auto& data: m_aircraftData)
+	for (const auto& data : m_aircraftData)
 	{
-		Debug::log("Data Aircraft Type: " + Utility::aircraftTypeToString(data.type));
-		if(data.type == getAircraftType())
+		if (data.type == getAircraftType())
 		{
 			m_health = data.health;
 			m_speed = data.speed;
@@ -206,24 +201,25 @@ void Aircraft::loadResources()
 			setTextureLoadArea(data.textureLoadArea);
 		}
 	}
+
 	GameSprite::loadResources();
 
-	if(!(getNodeType() & static_cast<unsigned int>(NodeType::Player)))
+	if (!(getNodeType() & static_cast<unsigned int>(NodeType::Player)))
 	{
 		float vx = 0;
 		float vy = 0;
 		switch (m_enterDirection)
 		{
 		case Direction::North:
-			vx = -100;
-			vy = 1000;
+			vx = -1000;
+			vy = -1000;
 			break;
 		case Direction::South:
 			vx = -1000;
-			vy = 500;
+			vy = -1000;
 			break;
 		case Direction::East:
-			vx = -1500;
+			vx = -2100;
 			vy = 500;
 			break;
 		case Direction::West:
--- Game/Aircraft.h
+++ Game/Aircraft.h
@@ -8,42 +8,61 @@
 #include "../Engine/GameSprite.h"
 #include "DataTables.h"
 #include "../Engine/GameText.h"
+#include <vector>
 
 class Aircraft : public GameSprite
 {
  public:
 	explicit Aircraft(const bool hasCollision, sf::Vector2f scale, sf::Vector2f position);
 	virtual ~Aircraft() override = default;
-	int getHealth() { return m_health; }
+
+	int getHealth() const { return m_health; }
+	void setHealth(int health) { m_health = health; }
+
 	virtual void fireBullet(sf::Vector2f velocity);
 	void accelerate(sf::Vector2f velocity) { setVelocity(getVelocity() + velocity); }
-	void accelerate(float x, float y) { setVelocity(getVelocity() + sf::Vector2f(x,y)); }
-	float getMaxSpeed() { return m_speed; }
+	void accelerate(float x, float y) { setVelocity(getVelocity() + sf::Vector2f(x, y)); }
+
+	float getMaxSpeed() const { return m_speed; }
 	virtual AircraftType getAircraftType() = 0;
+
 	virtual void loadResources() override;
+
  protected:
 	virtual void handleAnimation(float deltaTime);
 	void handleDamageAnimation(float deltaTime);
 	virtual void takeDamage(int health);
 	virtual void update(float deltaTime) override;
-	void setHealth(int health) { m_health = health; }
+
 	virtual unsigned int getNodeType() const override { return GameSprite::getNodeType() | (unsigned int)NodeType::Aircraft; }
-	static std::vector<AircraftData> m_aircraftData;
+
  private:
-	float m_timeSinceDamage;
-	bool m_isDamageAnimationActive;
-	bool m_isMovingToStartPos;
-	bool m_isExiting;
+	void updateHealthDisplay();
+	void updatePosition(float deltaTime);
+	void moveTowardsStart(float deltaTime);
+	void exitPhase();
+	void followAiRoutines(float deltaTime);
+	sf::Vector2f calculateDirectionalVelocity(Direction direction) const;
+
+	static std::vector<AircraftData> m_aircraftData;
+
 	int m_health;
 	int m_speed;
+	float m_timeSinceDamage;
+	float m_fireCooldownRemaining;
 	float m_routineDistanceTravelled;
 	float m_spawnDistanceTravelled;
+
+	bool m_isDamageAnimationActive;
+	bool m_isExiting;
+
 	int m_routineIndex;
-	float m_fireCooldownRemaining;
+	float m_despawnDistance;
+
 	GameText* m_healthDisplay;
 	std::vector<AiRoutine> m_aiRoutines;
+
 	sf::Vector2f m_spawnPos;
-	float m_despawnDistance;
 	Direction m_enterDirection;
 	Direction m_exitDirection;
 };
